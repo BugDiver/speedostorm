7c4cba2630524e35054da2fa1cf0f7be68903489
BugDiver <vinaysh@thoughtworks.com>
2016-03-08 15:04:56 +0530

    basic enemies are ready to hit the car

--- game.py
+++ game.py
@@ -1,9 +1,11 @@
 import pygame
+import time
+import random
 
 pygame.init()
 
-display_width = 900
-display_height = 600
+display_width = 800
+display_height = 900
 car_width  = 140
 
 black = (0,0,0)
@@ -14,23 +16,52 @@ red = (255,0,0)
 
 gameDisplay = pygame.display.set_mode((display_width,display_height))
 pygame.display.set_caption('SpeedoStorm')
-clock = pygame.time.Clock()
 
+clock = pygame.time.Clock()
 carImg = pygame.image.load('racingcar.png')
+
 def car(x,y):
 	gameDisplay.blit(carImg ,(x,y))
 
+def enemy(enemy_x,enemy_y,enemy_w,enemy_h,color):
+	pygame.draw.circle(gameDisplay ,color ,[enemy_x,enemy_y,enemy_w,enemy_h])
+	
+
+def text_objects(text ,font):
+	textSurface = font.render(text ,True ,red)
+	return textSurface,textSurface.get_rect()
+
+def display_message(message):
+	largeText = pygame.font.Font('freesansbold.ttf',115)
+	TextSurface ,TextRect = text_objects(message ,largeText)
+	TextRect.center  =((display_width /2),(display_height /2))
+	gameDisplay.blit(TextSurface ,TextRect)
+	pygame.display.update()
+
+	time.sleep(2)
+	game_loop()
+
+def crash():
+	display_message('You Crashed!!!')
 
 def game_loop():
 	x = (display_width * 0.43)
 	y  =(display_height * 0.8)
 	x_change = 0
+	enemy_start_x = random.randrange(0,display_width)
+	enemy_start_y = -600
+	enemy_speed  = 10
+	enemy_width = 100
+	enemy_height = 100
+
+
 	gameExit = False
 
 	while not gameExit:
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
-				gameExit = True
+				pygame.quit()
+				quit()
 			if event.type == pygame.KEYDOWN:
 				if event.key == pygame.K_LEFT:
 					x_change = -5
@@ -43,10 +74,18 @@ def game_loop():
 				
 		x += x_change
 		gameDisplay.fill(white)
-		car(x,y)
-		if x > display_width -car_width or x < 0:
-			gameExit = True
 
+
+		car(x,y)
+
+		enemy(enemy_start_x,enemy_start_y ,enemy_width ,enemy_height ,black)
+		enemy_start_y += enemy_speed
+
+		if x > display_width -car_width or x < 0:
+			crash()
+		if enemy_start_y > display_height:
+			enemy_start_y = -enemy_height
+			enemy_start_x = random.randrange(0,display_width)
 		pygame.display.update()
 		clock.tick(60)
 
