08df3e732248a60bd14e2ffef49970edfece6795
BugDiver <vinaysh@thoughtworks.com>
2016-03-08 14:04:20 +0530

    car can move and also crash by touching edge

--- game.py
+++ game.py
@@ -4,6 +4,7 @@ pygame.init()
 
 display_width = 900
 display_height = 600
+car_width  = 140
 
 black = (0,0,0)
 white = (255,255,255)
@@ -16,25 +17,40 @@ pygame.display.set_caption('SpeedoStorm')
 clock = pygame.time.Clock()
 
 carImg = pygame.image.load('racingcar.png')
-
 def car(x,y):
 	gameDisplay.blit(carImg ,(x,y))
 
-x = (display_width * 0.43)
-y  =(display_height * 0.8)
+
+def game_loop():
+	x = (display_width * 0.43)
+	y  =(display_height * 0.8)
+	x_change = 0
+	gameExit = False
+
+	while not gameExit:
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				gameExit = True
+			if event.type == pygame.KEYDOWN:
+				if event.key == pygame.K_LEFT:
+					x_change = -5
+				if event.key == pygame.K_RIGHT:
+					x_change = 5
+			if event.type == pygame.KEYUP:
+				if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+					x_change = 0
+					
+				
+		x += x_change
+		gameDisplay.fill(white)
+		car(x,y)
+		if x > display_width -car_width or x < 0:
+			gameExit = True
+
+		pygame.display.update()
+		clock.tick(60)
 
 
-crashed = False
-
-while not crashed:
-	for event in pygame.event.get():
-		if event.type == pygame.QUIT:
-			crashed = True
-
-	gameDisplay.fill(white)
-	car(x,y)
-	pygame.display.update()
-	clock.tick(60)
-
+game_loop()
 pygame.quit()
 quit()
